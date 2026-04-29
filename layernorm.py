c4b44fe2e555b220d8c5baac52a053238c8fae04
AnPay <42369232+AnPay@users.noreply.github.com>
2026-04-29 22:37:18 +0530

    Update layernorm.py-rmsnorm

--- layernorm.py
+++ layernorm.py
@@ -15,3 +15,23 @@ def layer_normalization(X: np.ndarray, gamma: np.ndarray, beta: np.ndarray, epsi
     # scale and shift
 	out = gamma * x_hat + beta
 	return out
+
+
+import numpy as np
+from typing import List
+
+'''
+
+'''
+class Solution:
+    def rms_norm(self, x: List[float], gamma: List[float], eps: float) -> List[float]:
+        # Implement RMS Normalization (similar to LayerNorm but without mean centering or beta)
+        # Normalize x, then scale by gamma
+        # Return result rounded to 4 decimal places as a list
+        x = np.array(x)
+        gamma = np.array(gamma)
+        mean_square = np.mean(x**2,axis=-1,keepdims=True)
+        # normalize
+        x_hat = (x) / np.sqrt(mean_square+eps)
+        output = x_hat*gamma
+        return np.round(output,4).tolist()
